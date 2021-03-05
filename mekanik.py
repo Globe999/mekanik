54c986ec2dd401cff310002a32a3ce1b16258529
Globee <marten.sanderod@gmail.com>
2021-03-05 14:55:19 +0100

    Finished lab

--- mekanik.py
+++ mekanik.py
@@ -6,48 +6,39 @@ import numpy as np
 import math as m
 
 
-useDummy = True
 a = 9
 b = 1
-xi0 = np.array([0.5]) #Startplats för xi
-w0 = 3 #Rad per sekund
+xi0 = np.array([0.5,0]) #Startplats för xi
+w0 = 13	#Rad per sekund
 g = 9.81 #Gravitation
 
 
-#DUMMY
-y0 = np.array([0.5])
-def dummyPrim(t, xi):
-	return np.array([m.pow(m.sin(xi),2)])
-
-def dummy2(t, xi):
-	return None
-
-
-#REAL VALUES
-
-
-def f(t,xi):
+def f(xi):
 	return a * m.pow(m.sin(xi), 2)+ b*m.pow(xi, 2)
 
-def prim(t, xi):
-	return 2*a*m.sin(xi)*m.cos(xi)+2*b
+def fPrim(xi):
+	return 2*a*m.sin(xi)*m.cos(xi)+2*b*xi
 
-def biss(xi):
+def fBiss(xi):
 	return 2*(a*m.pow(m.cos(xi), 2)-a*m.pow(m.sin(xi),2)+b)
 
-#initial vid tiden 0
+
+def func(t,xi):
+	xiPrim=[0,0]
+	xiPrim[0]=xi[1]
+	xiPrim[1] = (m.pow(w0,2)*xi[0]-fPrim(xi[0])*fBiss(xi[0])*m.pow(xi[1],2)-fPrim(xi[0])*g)/(m.pow(fPrim(xi[0]),2)+1)
+	return xiPrim
+
 #stop time
 timeSpan = np.array([0, 10])
-tend = 10
+
 
 
 # solve ODE
 #solve_ivp(<fn name>, <timespan[]>, <y0>)
-sol = None
-if useDummy:
-	sol = solve_ivp(dummyPrim, timeSpan, y0, method="Radau", t_eval=np.linspace(0,tend,10000))
-else:
-	sol = solve_ivp(prim, timeSpan, xi0, t_eval=np.linspace(0,tend,10000))
+
+sol = solve_ivp(func, timeSpan, [0.5,0], method="Radau", t_eval=np.linspace(0,timeSpan[1],10000))
+	# sol2 = solve_ivp(biss, timeSpan, xi0, method="Radau", t_eval=np.linspace(0,tend,10000))
 print(sol)
 #plot results
 plt.plot(sol.t,sol.y[0,:])
