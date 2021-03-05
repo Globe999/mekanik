30049b263c66680c67b65e2b276240a1fbb0b25f
Globee <marten.sanderod@gmail.com>
2021-03-05 15:22:57 +0100

    Added graphs to code

--- mekanik.py
+++ mekanik.py
@@ -9,7 +9,7 @@ import math as m
 a = 9
 b = 1
 xi0 = np.array([0.5,0]) #Startplats för xi
-w0 = 13	#Rad per sekund
+w0 = 0	#Rad per sekund
 g = 9.81 #Gravitation
 
 
@@ -38,10 +38,24 @@ timeSpan = np.array([0, 10])
 #solve_ivp(<fn name>, <timespan[]>, <y0>)
 
 sol = solve_ivp(func, timeSpan, [0.5,0], method="Radau", t_eval=np.linspace(0,timeSpan[1],10000))
-	# sol2 = solve_ivp(biss, timeSpan, xi0, method="Radau", t_eval=np.linspace(0,tend,10000))
-print(sol)
+w0=12.8
+sol2 = solve_ivp(func, timeSpan, [0.5,0], method="Radau", t_eval=np.linspace(0,timeSpan[1],10000))	
+w0=13
+sol3 = solve_ivp(func, timeSpan, [0.5,0], method="Radau", t_eval=np.linspace(0,timeSpan[1],10000))	
+# sol2 = solve_ivp(biss, timeSpan, xi0, method="Radau", t_eval=np.linspace(0,tend,10000)
 #plot results
-plt.plot(sol.t,sol.y[0,:])
-plt.xlabel("time")
-plt.ylabel("eta(t)")
+fig,(ax1,ax2,ax3) = plt.subplots(3)
+fig.suptitle("Xi(biss) vid w0=3 samt w0=13")
+ax1.plot(sol.t,sol.y[0,:])
+ax1.set_title("Omega = 3")
+# ax1.xlabel("Time")
+# ax1.ylabel("Eta(t)")
+
+ax2.plot(sol2.t,sol2.y[0,:])
+ax2.set_title("Omega = 12.8")
+ax3.plot(sol3.t,sol3.y[0,:])
+ax3.set_title("Omega = 13")
+# ax2.xlabel("Time")
+# ax2.ylabel("Eta(t)")
+
 plt.show()
